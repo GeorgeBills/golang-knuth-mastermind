578d88f07956e1d7cc782b9eec68552f5119c240
George Bills <george.s.bills@gmail.com>
2019-07-07 09:42:45 +1000

    assess() function.

--- code.go
+++ code.go
@@ -32,3 +32,35 @@ const numSlots = 4
 func (c code) String() string {
 	return strings.Join([]string{c.slot1.String(), c.slot2.String(), c.slot3.String(), c.slot4.String()}, " ")
 }
+
+func (c code) assess(guess code) feedback {
+	f := feedback{}
+
+	// count the colours in the code
+	var colourCount [maxCodePeg + 1]uint8
+	colourCount[c.slot1]++
+	colourCount[c.slot2]++
+	colourCount[c.slot3]++
+	colourCount[c.slot4]++
+
+	// assess each code peg in the guess
+	assessSlot := func(guess, real codePeg) {
+		if guess == real {
+			// if the guess code peg matches the real code peg in the same slot,
+			// then we indicate that with a black key peg
+			f.numBlack++
+			colourCount[guess]--
+		} else if colourCount[guess] > 0 {
+			// if the guess peg matches one of the colours we have, but wasn't
+			// in the correct slot, then we indicate that with a white key peg
+			f.numWhite++
+			colourCount[guess]--
+		}
+	}
+	assessSlot(guess.slot1, c.slot1)
+	assessSlot(guess.slot2, c.slot2)
+	assessSlot(guess.slot3, c.slot3)
+	assessSlot(guess.slot4, c.slot4)
+
+	return f
+}
---
+++ code_test.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestAssess(t *testing.T) {
+	tests := []struct {
+		name        string
+		code, guess code
+		expected    feedback
+	}{
+		{
+			name:     "all incorrect",
+			code:     code{cpRed, cpGreen, cpYellow, cpBlue},
+			guess:    code{cpBlack, cpBlack, cpBlack, cpBlack},
+			expected: feedback{numBlack: 0, numWhite: 0},
+		},
+		{
+			name:     "all correct colours and correct positions",
+			code:     code{cpRed, cpGreen, cpYellow, cpBlue},
+			guess:    code{cpRed, cpGreen, cpYellow, cpBlue},
+			expected: feedback{numBlack: 4, numWhite: 0},
+		},
+		{
+			name:     "all correct colours, but incorrect positions",
+			code:     code{cpRed, cpGreen, cpYellow, cpBlue},
+			guess:    code{cpBlue, cpYellow, cpGreen, cpRed},
+			expected: feedback{numBlack: 0, numWhite: 4},
+		},
+		{
+			name:     "one guess shouldn't give two key pegs",
+			code:     code{cpRed, cpRed, cpBlue, cpGreen},
+			guess:    code{cpRed, cpRed, cpRed, cpRed},
+			expected: feedback{numBlack: 2, numWhite: 0},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			feedback := tt.code.assess(tt.guess)
+			if feedback != tt.expected {
+				t.Errorf("Expected %+v but was %+v", tt.expected, feedback)
+			}
+		})
+	}
+}
